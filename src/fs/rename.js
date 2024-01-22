88198b10f02ee9b612eea285e77084b08d37309c
Anna Biruk <49333189+anna-biruk@users.noreply.github.com>
2024-01-22 15:12:57 +0100

    implemented rename method

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import fs from 'fs'
+
+const wrongFilenamePath = "./fs/files/wrongFilename.txt";
+const properFilenamePath = "./fs/files/properFilename.md";
+
+
 const rename = async () => {
-    // Write your code here 
+    if (!fs.existsSync(wrongFilenamePath) || fs.existsSync(properFilenamePath)) {
+        throw new Error('FS operation failed');
+    }
+
+    await fs.promises.rename(wrongFilenamePath, properFilenamePath)
+    console.log("File renamed successfully")
 };
 
 await rename();
