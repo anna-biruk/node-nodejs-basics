5d9348fffccefdf91bc304d0fe1efc01a6db4315
Anna Biruk <49333189+anna-biruk@users.noreply.github.com>
2024-01-22 14:16:30 +0100

    implemented create method

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+
+const filePath = "./fs/files/fresh.txt";
+
+
 const create = async () => {
-    // Write your code here 
+    if (fs.existsSync(filePath)) {
+        throw new Error('File already exists');
+    }
+
+    await fs.promises.writeFile(filePath, "I am fresh and young", { flags: "w+" });
+    console.log('File is created successfully.');
 };
 
-await create();
+await create();     
