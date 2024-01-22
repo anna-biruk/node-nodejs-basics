2c9c5fee1d625e82619e8dc22c62b49047384bf0
Anna Biruk <49333189+anna-biruk@users.noreply.github.com>
2024-01-22 14:52:12 +0100

    implemented copy method

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import fs from 'fs'
+
+const sourceFolderPath = './fs/files';
+const destinationFolderPath = './fs/files_copy';
+
 const copy = async () => {
-    // Write your code here 
+    if (!fs.existsSync(sourceFolderPath) || fs.existsSync(destinationFolderPath)) {
+        throw new Error('FS operation failed');
+    }
+    fs.cpSync(sourceFolderPath, destinationFolderPath, {
+        recursive: true,
+    });
+    console.log('Files copied successfully.');
+
 };
 
 await copy();
