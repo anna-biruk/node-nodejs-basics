e6e8d59974d055d9fddbe36e5a4c4a1e72594afb
Anna Biruk <49333189+anna-biruk@users.noreply.github.com>
2024-01-24 17:07:17 +0100

    implemented zip methods

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
+const filePath = './zip/files/fileToCompress.txt';
+
 const compress = async () => {
-    // Write your code here 
+    const outputFile = "./zip/files/archive.gz"
+
+    const readStream = fs.createReadStream(filePath);
+    const writeStream = fs.createWriteStream(outputFile)
+
+    const gzip = zlib.createGzip()
+
+    readStream.pipe(gzip).pipe(writeStream);
+
+    writeStream.on('finish', () => {
+        console.log(`File compressed successfully to ${outputFile}`);
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import fs from 'fs'
+import zlib from 'zlib'
+
 const decompress = async () => {
-    // Write your code here 
+    const fileToDecompress = './zip/files/archive.gz'
+    const outputFile = "./zip/files/fileToCompress.txt"
+
+    const readStream = fs.createReadStream(fileToDecompress);
+    const writeStream = fs.createWriteStream(outputFile, 'utf-8')
+
+    const gunzip = zlib.createGunzip();
+
+    readStream.pipe(gunzip).pipe(writeStream);
+
+    writeStream.on('finish', () => {
+        console.log(`File decompressed successfully to ${outputFile}`);
+    });
 };
 
 await decompress();
